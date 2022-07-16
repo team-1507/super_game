d0d3f713707f3e5eb7a70b6589d0f9996c4c0340
Anastasiia <aabagaeva>
2022-07-16 13:53:40 +0300

    add page game-over

--- src/pages/game-over/GameOver.tsx
+++ src/pages/game-over/GameOver.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+import './game-over.scss';
 
 const GameOver = () => {
-    return <h1>GAME OVER</h1>;
+    const score = 9432;
+    return (
+        <div className="game-over-page">
+            <div className="game-over-page__title">
+                <h3>Game over</h3>
+            </div>
+            <div className="game-over-page__score">
+                <h3>
+                    Overall score:
+                    {score}
+                </h3>
+            </div>
+            <div className="home-page__buttons-group">
+                <div className="home-page__buttons-group__two-button__btn">
+                    <Button type="primary">
+                        START AGAIN
+                    </Button>
+                </div>
+                <div className="home-page__buttons-group__two-button__btn">
+                    <Link to="/">
+                        <Button type="primary">
+                            MAIN PAGE
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default GameOver;
