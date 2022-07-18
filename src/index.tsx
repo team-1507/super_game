17d15ff986cf915fc745a85c17545ff96a6c6467
Anastasia Bagaeva <72441267+Anastasia226@users.noreply.github.com>
2022-07-18 22:05:20 +0300

    Feature/task 5 10_main_page (#4)
    * add main page
    
    * fix after review
    
    Co-authored-by: Anastasiia <aabagaeva>

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import createRoot from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 
-ReactDOM.render(
+createRoot.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
