ea2497cd988308a72ad80a0e34bd3677dabba239
Kirill <sincerely.manny@gmail.com>
2022-08-03 15:00:08 +0300

    commented tile size constant

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -1,5 +1,9 @@
 export const MAP_SIZE: [number, number] = [20, 20]; // rows, cols (in tiles)
 
+/*
+Этой константой можно менять размер тайла и, соответственно, масштаб на странице
+Остальные размеры он сам подгонит и пересчитает.
+*/
 export const TILE_SIZE = 64; // in px
 
 export const MOVES: Record<string, [number, number]> = {
