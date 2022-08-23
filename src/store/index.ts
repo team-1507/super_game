70e3e84a948dffa77d65ea32aee3fc618b62cd32
Anastasia Bagaeva <72441267+Anastasia226@users.noreply.github.com>
2022-08-23 15:26:12 +0300

    Feature/task 5 22 redux thunk (#23)
    * add redux thunk
    
    * add thunk
    
    * fix
    
    Co-authored-by: Anastasiia <aabagaeva>

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,7 @@ import rootReducer from './reducers';
 const store = configureStore({
     reducer: rootReducer,
     devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export default store;
