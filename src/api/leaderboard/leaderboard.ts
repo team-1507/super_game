5b3ea57bcfc039498990e87a1f26c25d1c57af50
Alexey Kravchenko <a.kravchenko@rt-solar.ru>
2022-08-29 00:10:11 +0300

    [task-7-34] init Leaderboard API

---
+++ src/api/leaderboard/leaderboard.ts
@@ -0,0 +1,28 @@
+import ApiService from '../api';
+import { TEAM_NAME } from './constants';
+import {
+    SetLeaderboardData,
+    GetLeaderboardData,
+    SetLeaderboardDataDto,
+    GetLeaderboardDataDto,
+} from './types';
+
+function setPlayerScoreToLeaderboard({ teamName = TEAM_NAME, ...fields }: SetLeaderboardData): Promise<SetLeaderboardDataDto | null> {
+    const body = Object.assign(fields, { teamName });
+    return ApiService.postRequest<SetLeaderboardData, SetLeaderboardDataDto>({
+        address: `leaderboard`,
+        body,
+    });
+};
+
+function getLeaderboard(body: GetLeaderboardData): Promise<GetLeaderboardDataDto[] | null> {
+    return ApiService.postRequest<GetLeaderboardData, any>({
+        address: `leaderboard/${TEAM_NAME}`,
+        body,
+    });
+};
+
+export default {
+    setPlayerScoreToLeaderboard,
+    getLeaderboard,
+};
--- src/pages/leaderboard/Leaderboard.tsx
+++ src/pages/leaderboard/Leaderboard.tsx
@@ -1,20 +1,57 @@
 import React from 'react';
-import './Leaderboard.scss';
 import users from './users';
 import HeaderBackButton from '../../components/sections/HeaderBackButton';
+import LeaderboardApi from '../../api/leaderboard/leaderboard';
+import { CURSOR, LIMIT, RATING_FIELD_NAME } from '../../api/leaderboard/constants';
+import { Button } from 'antd';
+import './Leaderboard.scss';
+
+// Убрать после успешного тестирования апишки результатов
+import { useAppSelector } from '../../store/hooks';
+import { TEAM_NAME } from '../../api/leaderboard/constants';
+//
 
 const Leaderboard = () => {
-    const generateCols = users
-        .map((item) => (
-            <li key={item}>{item}</li>
-        ));
+    const user = useAppSelector((state) => state.user);
+    let items = users;
+    const generateCols = (items: any) => {
+        return items
+            .map(({ name }: any) => (
+                <li key={name}>{name}</li>
+            ));
+    }
+    const body = { cursor: CURSOR, limit: LIMIT, ratingFieldName: RATING_FIELD_NAME };
+    LeaderboardApi.getLeaderboard(body)
+        .then((res) => {
+            console.warn(res);
+            items = res || [];
+        });
+    const setLeaders = () => {
+        const { login } = user;
+        if (!login) return;
+        const body = {
+            data: {
+                name: login,
+                score: 2,
+                time: 1
+            },
+            ratingFieldName: RATING_FIELD_NAME,
+            teamName: TEAM_NAME,
+        };
+        LeaderboardApi.setPlayerScoreToLeaderboard(body);
+    };
+
 
     return (
         <main className="leaderboard-page">
             <HeaderBackButton header="Leaderboard" />
+            {/* Временная кнопка добавления результатов */}
+            <Button htmlType="button" type="primary" onClick={setLeaders}>
+                Set my super result
+            </Button>
             <section className="leaderboard-page__grid">
                 <ol className="leaders-list">
-                    {generateCols}
+                    {generateCols(items)}
                 </ol>
             </section>
         </main>
