8b88a3e7bc7eb8d829d9ee440504296e32589c01
Alexey Kravchenko <a.kravchenko@rt-solar.ru>
2022-09-04 17:42:35 +0300

    merge from 8-39

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,6 +1,4 @@
 import React, { createRef, useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from '../../store/hooks';
-import { togglePause } from '../../game/store/uiSlice';
 import Character from '../../game/character/Character';
 import ControlsWrapper from '../../game/controls/ControlsWrapper';
 import Map from '../../game/map/Map';
@@ -27,10 +25,6 @@ const Game = () => {
     const controlsWrapperRef = createRef<HTMLDivElement>();
     const characterRef = createRef<HTMLCanvasElement>();
     const gardenRef = createRef<HTMLCanvasElement>();
-    const { pauseMenu: isPause } = useAppSelector((state) => state.ui);
-    const dispatch = useAppDispatch();
-
-    const togglePauseMenu = () => dispatch(togglePause());
 
     useEffect(() => {
         controlsWrapperRef.current?.focus();
@@ -51,7 +45,7 @@ const Game = () => {
                 </div>
                 <Interface />
                 <ActionButtonsWithControls />
-                <PauseMenu active={isPause} wrapperRef={pauseMenu} toggleFn={togglePauseMenu} />
+                <PauseMenu wrapperRef={pauseMenu} />
             </ControlsWrapperWithControls>
         </main>
     );
