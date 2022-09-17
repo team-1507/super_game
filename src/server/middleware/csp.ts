d205029dbfc78d224b0836e774686e66c9bdbb2d
Anastasiia <aabagaeva>
2022-09-17 18:45:52 +0300

    fix

--- src/server/middleware/csp.ts
+++ src/server/middleware/csp.ts
@@ -1,13 +1,13 @@
 import {
-    DATA, EVAL, expressCspHeader, INLINE, SELF,
+    DATA, EVAL, expressCspHeader, INLINE, NONCE, SELF
 } from 'express-csp-header';
 
 export const csp = () => expressCspHeader({
     directives: {
-        'default-src': [SELF, 'https://ya-praktikumrr.tech'],
-        'script-src': [SELF, INLINE, EVAL],
+        'default-src': [SELF, 'https://ya-praktikum.tech'],
+        'script-src': [SELF, INLINE, EVAL, NONCE],
         'font-src': [SELF, DATA],
-        'img-src': [DATA, SELF, INLINE, 'https://ya-praktikumrr.tech'],
+        'img-src': [DATA, SELF, INLINE, 'https://ya-praktikum.tech'],
         'style-src': [SELF, INLINE],
         'worker-src': [SELF],
     },
