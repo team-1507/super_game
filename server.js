aa39df51c9bb7a885e9af8edbba0016fa5672dfa
Anastasia Bagaeva <72441267+Anastasia226@users.noreply.github.com>
2022-09-21 12:42:51 +0300

    Feature/task 9 50 (#43)
    * add csp
    
    * fix
    
    * delete unused package
    
    Co-authored-by: Anastasiia <aabagaeva>

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const { DATA, EVAL, expressCspHeader, INLINE, SELF, NONCE }  = require('express-csp-header')
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(`${__dirname}/build`));
+app.use(expressCspHeader({
+    directives: {
+        'default-src': [SELF, 'https://ya-praktikum.tech'],
+        'script-src': [SELF, INLINE, EVAL, NONCE],
+        'font-src': [SELF, DATA],
+        'img-src': [DATA, SELF, INLINE, 'https://ya-praktikum.tech'],
+        'style-src': [SELF, INLINE],
+        'worker-src': [SELF],
+    }
+}));
 
 app.get('/*', (_req, res) => {
     res.sendFile(`${__dirname}/build/index.html`);
